38860e3cbec379f08cb0a10fe75be87fbad47d98
Ken Kubiak <kubiak@calpoly.edu>
2024-11-03 10:15:49 -0800

    Finish Lesson 1 of ComposeTutorial

---
+++ ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/MainActivity.kt
@@ -0,0 +1,28 @@
+package dev.csse.kubiak.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard( "SLO")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("CSC 436")
+}
