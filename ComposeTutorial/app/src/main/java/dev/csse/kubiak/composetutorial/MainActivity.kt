fdfb3055f2277fd5b70fc361c45857a8f69a3d01
Ken Kubiak <kubiak@calpoly.edu>
2024-11-03 10:42:22 -0800

    finish Lesson 2 of ComposeTutorial

--- ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/MainActivity.kt
@@ -3,26 +3,52 @@ package dev.csse.kubiak.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard( "SLO")
+            MessageCard( Message(body = "Hello, SLO", author = "Prof. Kubiak"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message ) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.red_kurta),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier.size(40.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.body)
+            Text(text = "by ${msg.author}")
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("CSC 436")
+    MessageCard(Message("CSC 436", "We are now in Winter 2025"))
 }
