f95049388e76cced7547b5d47e0a823630f6feda
Ken Kubiak <kubiak@calpoly.edu>
2024-11-11 15:48:30 -0800

    finish compose tutorial

--- ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/MainActivity.kt
@@ -1,30 +1,48 @@
 package dev.csse.kubiak.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import dev.csse.kubiak.composetutorial.ui.theme.ComposeTutorialTheme
+// import dev.csse.kubiak.composetutorial.SampleData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard( Message(body = "Hello, SLO", author = "Prof. Kubiak"))
+            ComposeTutorialTheme {
+                Conversation(SampleData.conversationSample)
+            }
         }
     }
 }
@@ -32,23 +50,71 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(msg: Message ) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.red_kurta),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier.size(40.dp).clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = msg.body)
-            Text(text = "by ${msg.author}")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message ) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Image(
+            painter = painterResource(R.drawable.red_kurta),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier.size(40.dp).clip(CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.primary)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(text = "by ${msg.author}",
+                color = MaterialTheme.colorScheme.tertiary,
+                style = MaterialTheme.typography.titleSmall)
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 8.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)@Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("CSC 436", "We are now in Winter 2025"))
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("CSC 436", "We are now in Winter 2025")
+            )
+        }
+    }
 }
--- ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/ui/theme/Type.kt
+++ ComposeTutorial/app/src/main/java/dev/csse/kubiak/composetutorial/ui/theme/Type.kt
@@ -14,13 +14,26 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
@@ -30,5 +43,4 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
