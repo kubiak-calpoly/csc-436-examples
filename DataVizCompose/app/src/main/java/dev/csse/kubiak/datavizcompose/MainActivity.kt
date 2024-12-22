7f6266ad40cc7b67be3e309c341688e906503b0d
Ken Kubiak <kubiak@calpoly.edu>
2024-12-22 09:12:24 -0800

    Use Shape for clipping

--- DataVizCompose/app/src/main/java/dev/csse/kubiak/datavizcompose/MainActivity.kt
+++ DataVizCompose/app/src/main/java/dev/csse/kubiak/datavizcompose/MainActivity.kt
@@ -1,30 +1,55 @@
 package dev.csse.kubiak.datavizcompose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.Morph
 import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.star
 import androidx.graphics.shapes.toPath
 import dev.csse.kubiak.datavizcompose.ui.theme.DataVizComposeTheme
+import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,52 +70,95 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DataViz(name: String, modifier: Modifier = Modifier) {
-  val infiniteAnimation = rememberInfiniteTransition(label = "infinite animation")
-  val morphProgress = infiniteAnimation.animateFloat(
-    initialValue = 0f,
-    targetValue = 1f,
-    animationSpec = infiniteRepeatable(
-      tween(500),
-      repeatMode = RepeatMode.Reverse
-    ),
-    label = "morph"
+  val shapeA = remember {
+    RoundedPolygon(
+      6,
+      rounding = CornerRounding(0.2f)
+    )
+  }
+  val shapeB = remember {
+    RoundedPolygon.star(
+      6,
+      rounding = CornerRounding(0.1f)
+    )
+  }
+  val morph = remember {
+    Morph(shapeA, shapeB)
+  }
+  val interactionSource = remember {
+    MutableInteractionSource()
+  }
+  val isPressed by interactionSource.collectIsPressedAsState()
+  val animatedProgress = animateFloatAsState(
+    targetValue = if (isPressed) 1f else 0f,
+    label = "progress",
+    animationSpec = spring(dampingRatio = 0.4f, stiffness = Spring.StiffnessMedium)
   )
   Box(
     modifier = Modifier
-      .drawWithCache {
-        val triangle = RoundedPolygon(
-          numVertices = 3,
-          radius = size.minDimension / 2f,
-          centerX = size.width / 2f,
-          centerY = size.height / 2f,
-          rounding = CornerRounding(
-            size.minDimension / 10f,
-            smoothing = 0.1f
-          )
-        )
-        val square = RoundedPolygon(
-          numVertices = 4,
-          radius = size.minDimension / 2f,
-          centerX = size.width / 2f,
-          centerY = size.height / 2f
-        )
-
-        val morph = Morph(start = triangle, end = square)
-        val morphPath = morph
-          .toPath(progress = morphProgress.value).asComposePath()
-
-        onDrawBehind {
-          drawPath(morphPath, color = Color.Black)
-        }
+      .size(200.dp)
+      .padding(8.dp)
+      .clip(MorphPolygonShape(morph, animatedProgress.value))
+      .background(Color(0xFF80DEEA))
+      .size(200.dp)
+      .clickable(interactionSource = interactionSource, indication = null) {
       }
-      .fillMaxSize()
-  )
+  ) {
+    Text("Hello", modifier = Modifier.align(Alignment.Center))
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DataVizPreview() {
   DataVizComposeTheme {
     DataViz("Android")
+  }
+}
+
+fun RoundedPolygon.getBounds() = calculateBounds().let {
+  Rect(it[0], it[1], it[2], it[3]) }
+class RoundedPolygonShape(
+  private val polygon: RoundedPolygon,
+  private var matrix: Matrix = Matrix()
+) : Shape {
+  private var path = Path()
+  override fun createOutline(
+    size: Size,
+    layoutDirection: LayoutDirection,
+    density: Density
+  ): Outline {
+    path.rewind()
+    path = polygon.toPath().asComposePath()
+    matrix.reset()
+    val bounds = polygon.getBounds()
+    val maxDimension = max(bounds.width, bounds.height)
+    matrix.scale(size.width / maxDimension, size.height / maxDimension)
+    matrix.translate(-bounds.left, -bounds.top)
+
+    path.transform(matrix)
+    return Outline.Generic(path)
+  }
+}
+
+class MorphPolygonShape(
+  private val morph: Morph,
+  private val percentage: Float
+) : Shape {
+
+  private val matrix = Matrix()
+  override fun createOutline(
+    size: Size,
+    layoutDirection: LayoutDirection,
+    density: Density
+  ): Outline {
+    // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+    // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+    matrix.scale(size.width / 2f, size.height / 2f)
+    matrix.translate(1f, 1f)
+
+    val path = morph.toPath(progress = percentage).asComposePath()
+    path.transform(matrix)
+    return Outline.Generic(path)
   }
 }
