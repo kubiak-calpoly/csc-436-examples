39cccf3c7379e534a3bc90a747fd263148f1818d
Ken Kubiak <kubiak@calpoly.edu>
2024-12-22 09:23:21 -0800

    add animated rotating clip image

--- DataVizCompose/app/src/main/java/dev/csse/kubiak/datavizcompose/MainActivity.kt
+++ DataVizCompose/app/src/main/java/dev/csse/kubiak/datavizcompose/MainActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -30,6 +37,8 @@ import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
@@ -62,39 +71,56 @@ class MainActivity : ComponentActivity() {
 fun DataViz(name: String, modifier: Modifier = Modifier) {
   val shapeA = remember {
     RoundedPolygon(
-      6,
+      12,
       rounding = CornerRounding(0.2f)
     )
   }
   val shapeB = remember {
     RoundedPolygon.star(
-      6,
-      rounding = CornerRounding(0.1f)
+      12,
+      rounding = CornerRounding(0.2f)
     )
   }
   val morph = remember {
     Morph(shapeA, shapeB)
   }
-  val interactionSource = remember {
-    MutableInteractionSource()
-  }
-  val isPressed by interactionSource.collectIsPressedAsState()
-  val animatedProgress = animateFloatAsState(
-    targetValue = if (isPressed) 1f else 0f,
-    label = "progress",
-    animationSpec = spring(dampingRatio = 0.4f, stiffness = Spring.StiffnessMedium)
+  val infiniteTransition = rememberInfiniteTransition("infinite outline movement")
+  val animatedProgress = infiniteTransition.animateFloat(
+    initialValue = 0f,
+    targetValue = 1f,
+    animationSpec = infiniteRepeatable(
+      tween(2000, easing = LinearEasing),
+      repeatMode = RepeatMode.Reverse
+    ),
+    label = "animatedMorphProgress"
+  )
+  val animatedRotation = infiniteTransition.animateFloat(
+    initialValue = 0f,
+    targetValue = 360f,
+    animationSpec = infiniteRepeatable(
+      tween(6000, easing = LinearEasing),
+      repeatMode = RepeatMode.Reverse
+    ),
+    label = "animatedMorphProgress"
   )
   Box(
-    modifier = Modifier
-      .size(200.dp)
-      .padding(8.dp)
-      .clip(MorphPolygonShape(morph, animatedProgress.value))
-      .background(Color(0xFF80DEEA))
-      .size(200.dp)
-      .clickable(interactionSource = interactionSource, indication = null) {
-      }
+    modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center
   ) {
-    Text("Hello", modifier = Modifier.align(Alignment.Center))
+    Image(
+      painter = painterResource(id = R.drawable.dog),
+      contentDescription = "Dog",
+      contentScale = ContentScale.Crop,
+      modifier = Modifier
+        .clip(
+          CustomRotatingMorphShape(
+            morph,
+            animatedProgress.value,
+            animatedRotation.value
+          )
+        )
+        .size(200.dp)
+    )
   }
 }
 
@@ -124,6 +150,31 @@ class MorphPolygonShape(
 
     val path = morph.toPath(progress = percentage).asComposePath()
     path.transform(matrix)
+    return Outline.Generic(path)
+  }
+}
+
+class CustomRotatingMorphShape(
+  private val morph: Morph,
+  private val percentage: Float,
+  private val rotation: Float
+) : Shape {
+
+  private val matrix = Matrix()
+  override fun createOutline(
+    size: Size,
+    layoutDirection: LayoutDirection,
+    density: Density
+  ): Outline {
+    // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+    // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+    matrix.scale(size.width / 2f, size.height / 2f)
+    matrix.translate(1f, 1f)
+    matrix.rotateZ(rotation)
+
+    val path = morph.toPath(progress = percentage).asComposePath()
+    path.transform(matrix)
+
     return Outline.Generic(path)
   }
 }
