21313cf9a0252d64b06afe2771959ebaf223351a
Ken Kubiak <kubiak@calpoly.edu>
2025-01-12 14:32:38 -0800

    add logo preview

--- DemoWeek2/app/src/main/java/dev/csse/kubiak/demoweek2/MainActivity.kt
+++ DemoWeek2/app/src/main/java/dev/csse/kubiak/demoweek2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -16,6 +17,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -35,7 +38,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(names: List<String>): Unit {
   for ( name in names ) {
-    Text(text = "Hello $name!")
+    Text(text = stringResource(
+      R.string.greeting,
+      name))
   }
 }
 
@@ -56,7 +61,9 @@ fun Greeting1Preview() {
 @Composable
 fun ClickCounter(clicks: Int, onClick: () -> Unit) {
   Button(onClick = onClick) {
-    Text("I've been clicked $clicks times")
+    Text(
+      stringResource(R.string.click_count, clicks)
+    )
   }
 }
 
@@ -76,5 +83,15 @@ fun GradScreen() {
     fontSize = 50.sp,
     fontWeight = FontWeight.Bold,
     lineHeight = 50.sp
+  )
+}
+
+@Preview
+@Composable
+fun LogoPreview() {
+  Image(
+    painter = painterResource(R.drawable.mustangs_logo),
+    contentDescription = "Graduation cap",
+    alpha = 0.3f
   )
 }
