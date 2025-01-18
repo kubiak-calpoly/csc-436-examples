5395328204fff1aa2847f42f7feb27d4d6804a4e
Ken Kubiak <kubiak@calpoly.edu>
2025-01-18 11:44:25 -0800

    Start Demo week 3 Activity Lifecycle

---
+++ DemoWeek3/app/src/main/java/dev/csse/kubiak/demoweek3/MainActivity.kt
@@ -0,0 +1,78 @@
+package dev.csse.kubiak.demoweek3
+
+import android.nfc.Tag
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.csse.kubiak.demoweek3.ui.theme.DemoWeek3Theme
+
+private const val TAG = "LifeCycleDemo"
+
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    Log.i(TAG, "onCreate")
+    setContent {
+      var name: String by remember { mutableStateOf("Android") }
+      Column( modifier = Modifier.padding(24.dp) ) {
+        Greeting( name = name )
+        TextField( value = name,
+          onValueChange =  { x: String -> name = x },
+          label = { Text("What is your name?") }
+        )
+      }
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Log.i(TAG, "onStart")
+  }
+
+  override fun onResume() {
+    super.onResume()
+    Log.i(TAG, "onResume")
+  }
+
+  override fun onPause() {
+    super.onPause()
+    Log.i(TAG, "onPause")
+  }
+
+  override fun onStop() {
+    super.onStop()
+    Log.i(TAG, "onStop")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.i(TAG, "onDestroy")
+  }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
