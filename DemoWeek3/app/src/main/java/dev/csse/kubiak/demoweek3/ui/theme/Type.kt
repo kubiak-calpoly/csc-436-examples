d673ea6a13ce3f91d88e1abaa20d5065b47764d0
Ken Kubiak <kubiak@calpoly.edu>
2025-01-23 08:21:49 -0800

    add merriweather fonts to theme

--- DemoWeek3/app/src/main/java/dev/csse/kubiak/demoweek3/ui/theme/Type.kt
+++ DemoWeek3/app/src/main/java/dev/csse/kubiak/demoweek3/ui/theme/Type.kt
@@ -2,22 +2,30 @@ package dev.csse.kubiak.demoweek3.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.csse.kubiak.demoweek3.R
+
+val Merriweather = FontFamily(
+  Font(R.font.merriweather_regular, FontWeight.Normal),
+  Font(R.font.merriweather_light, FontWeight.Light),
+  Font(R.font.merriweather_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
   bodyLarge = TextStyle(
-    fontFamily = FontFamily.Serif,
+    fontFamily = Merriweather,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp,
     lineHeight = 24.sp,
     letterSpacing = 0.5.sp
   ),
   titleLarge = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
+    fontFamily = Merriweather,
+    fontWeight = FontWeight.Bold,
     fontSize = 22.sp,
     lineHeight = 28.sp,
     letterSpacing = 0.sp
