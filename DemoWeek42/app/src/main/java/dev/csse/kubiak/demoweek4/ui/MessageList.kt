0478adc47874ea31fd1cabf020c27feeb5e9ae72
Ken Kubiak <kubiak@calpoly.edu>
2025-01-25 16:45:49 -0800

    add sticky header to message list (lazy column)

--- DemoWeek42/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
+++ DemoWeek42/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
@@ -1,8 +1,14 @@
 package dev.csse.kubiak.demoweek4.ui
 
+import android.R
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -12,7 +18,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.csse.kubiak.demoweek4.ui.theme.DemoWeek4Theme
 
 data class Message(
@@ -22,16 +30,21 @@ data class Message(
 
 val TAG = "MessageList"
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MessageList(messages: List<Message>) {
-//  Column( modifier = Modifier
-//        .fillMaxHeight()
-//        .verticalScroll(rememberScrollState()) ) {
-//    messages.forEach { message ->
-//      MessageRow(message)
-//    }
-//  }
-  LazyColumn {
+  LazyColumn(modifier = Modifier.fillMaxWidth()) {
+    stickyHeader {
+      Row(modifier = Modifier.fillMaxWidth()
+        .background(Color(0x18000000))
+        .padding(10.dp)
+      ){
+
+        Text("Messages",
+          style = MaterialTheme.typography.titleLarge)
+      }
+
+    }
     items(messages) { message ->
       MessageRow(message)
     }
@@ -43,22 +56,20 @@ fun MessageList(messages: List<Message>) {
 fun MessageRow(message: Message) {
   Log.i(TAG, "Composing MessageRow ${message.index}")
   Text(message.text,
-    style = MaterialTheme.typography.titleLarge)
+    style = MaterialTheme.typography.titleLarge,
+    modifier = Modifier.padding(10.dp) )
 }
 
 @Preview
 @Composable
 fun MessageListPreview() {
+  val messages = Array<Message>(200) {
+      n -> Message("This is message $n", n)
+  }
+
   DemoWeek4Theme {
     Surface {
-      MessageList(sampleMessages())
+      MessageList(listOf(*messages))
     }
   }
-}
-
-fun sampleMessages(): List<Message> {
-  val messages = Array<Message>(200) {
-    n -> Message("This is message $n", n)
-  }
-  return listOf(*messages)
 }
