a8d7f2ac3c45953df3da2cbd457959f23717cd43
Ken Kubiak <kubiak@calpoly.edu>
2025-01-25 17:54:15 -0800

    implement delete completed tasks

--- DemoWeek42/app/src/main/java/dev/csse/kubiak/demoweek4/ui/ToDoScreen.kt
+++ DemoWeek42/app/src/main/java/dev/csse/kubiak/demoweek4/ui/ToDoScreen.kt
@@ -2,7 +2,9 @@ package dev.csse.kubiak.demoweek4.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,11 +14,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SwipeToDismiss
@@ -42,98 +49,119 @@ import dev.csse.kubiak.demoweek4.ui.theme.DemoWeek4Theme
 
 @Composable
 fun ToDoScreen(
-   modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-  TaskList( modifier = modifier.fillMaxSize() )
+  TaskList(modifier = modifier.fillMaxSize())
 }
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun TaskList(
-   modifier: Modifier = Modifier,
-   todoViewModel: ToDoViewModel = viewModel()
+  modifier: Modifier = Modifier,
+  todoViewModel: ToDoViewModel = viewModel()
 ) {
-   LazyColumn( modifier = modifier ) {
+  Box(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = modifier) {
       stickyHeader {
-         AddTaskInput { s -> todoViewModel.addTask(s) }
+        AddTaskInput { s -> todoViewModel.addTask(s) }
       }
       items(
-         items = todoViewModel.taskList
+        items = todoViewModel.taskList
       ) { task ->
         TaskCard(
           task = task,
           toggleCompleted = { t ->
-             todoViewModel.toggleTaskCompleted(t) }
+            todoViewModel.toggleTaskCompleted(t)
+          }
         )
       }
-   }
+    }
+
+    Row(
+      modifier = Modifier
+        .align(Alignment.BottomCenter)
+        .padding(10.dp)
+    ) {
+      Button(
+        onClick = { todoViewModel.deleteCompletedTasks() },
+        enabled = todoViewModel.completedTasksExist
+      ) {
+        Icon(
+          imageVector = Icons.Default.Delete,
+          contentDescription = "Delete completed tasks"
+        )
+        Text("Delete Completed Tasks")
+      }
+    }
+  }
 }
 
 @Composable
 fun AddTaskInput(onEnterTask: (String) -> Unit) {
-   val keyboardController = LocalSoftwareKeyboardController.current
-   var taskBody by remember { mutableStateOf("") }
+  val keyboardController = LocalSoftwareKeyboardController.current
+  var taskBody by remember { mutableStateOf("") }
 
-   OutlinedTextField(
-      modifier = Modifier
-         .fillMaxWidth()
-         .background(Color.White)
-         .padding(6.dp),
-      value = taskBody,
-      onValueChange = { taskBody = it },
-      label = { Text("Enter task") },
-      keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-      keyboardActions = KeyboardActions(
-         onDone = {
-            onEnterTask(taskBody)
-            taskBody = ""
-            keyboardController?.hide()
-         }
-      )
-   )
+  OutlinedTextField(
+    modifier = Modifier
+      .fillMaxWidth()
+      .background(Color.White)
+      .padding(6.dp),
+    value = taskBody,
+    onValueChange = { taskBody = it },
+    label = { Text("Enter task") },
+    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+    keyboardActions = KeyboardActions(
+      onDone = {
+        onEnterTask(taskBody)
+        taskBody = ""
+        keyboardController?.hide()
+      }
+    )
+  )
 }
 
 @Composable
 fun TaskCard(
-   task: Task,
-   toggleCompleted: (Task) -> Unit,
-   modifier: Modifier = Modifier
+  task: Task,
+  toggleCompleted: (Task) -> Unit,
+  modifier: Modifier = Modifier
 ) {
-   Card(
-      modifier = modifier
-         .padding(8.dp)
-         .fillMaxWidth(),
-      colors = CardDefaults.cardColors(
-         containerColor = MaterialTheme.colorScheme.surfaceVariant
+  Card(
+    modifier = modifier
+      .padding(8.dp)
+      .fillMaxWidth(),
+    colors = CardDefaults.cardColors(
+      containerColor = MaterialTheme.colorScheme.surfaceVariant
+    )
+  ) {
+    Row(
+      modifier = modifier.fillMaxWidth(),
+      verticalAlignment = Alignment.CenterVertically,
+      horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+      Checkbox(
+        checked = task.completed,
+        onCheckedChange = {
+          toggleCompleted(task)
+        }
       )
-   ) {
-      Row(modifier = modifier.fillMaxWidth(),
-         verticalAlignment = Alignment.CenterVertically,
-         horizontalArrangement = Arrangement.spacedBy(12.dp)
-      ) {
-         Checkbox(
-            checked = task.completed,
-            onCheckedChange = {
-               toggleCompleted(task)
-            }
-         )
-         Text(
-            text = task.body,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = modifier.padding(12.dp),
-            color = if (task.completed) Color.Gray else Color.Black
-         )
-      }
-   }
+      Text(
+        text = task.body,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = modifier.padding(12.dp),
+        color = if (task.completed) Color.Gray else Color.Black
+      )
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ToDoScreenPreview() {
-   val todoViewModel = viewModel<ToDoViewModel>()
-   todoViewModel.createTestTasks(5)
+  val todoViewModel = viewModel<ToDoViewModel>()
+  todoViewModel.createTestTasks(5)
 
-   DemoWeek4Theme(dynamicColor = false ) {
-      ToDoScreen()
-   }
+  DemoWeek4Theme(dynamicColor = false) {
+    ToDoScreen()
+  }
 }
