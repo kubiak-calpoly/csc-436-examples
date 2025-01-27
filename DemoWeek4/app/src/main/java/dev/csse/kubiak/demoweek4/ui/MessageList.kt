e2c3f37205dacc0d52fa4f5a5567a21607e9528d
Ken Kubiak <kubiak@calpoly.edu>
2025-01-27 12:05:45 -0800

    sticky header and footer

--- DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
+++ DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
@@ -1,13 +1,15 @@
 package dev.csse.kubiak.demoweek4.ui
 
-import android.R
-import android.nfc.Tag
-import android.telephony.TelephonyCallback
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -16,8 +18,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.csse.kubiak.demoweek4.ui.theme.DemoWeek4Theme
 
 data class Message(
@@ -27,21 +32,48 @@ data class Message(
 
 val TAG = "MessageList"
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MessageList(messages: List<Message>) {
-  LazyColumn {
-    items(messages) { message ->
-      MessageRow(message)
+  Box(modifier = Modifier.fillMaxSize()) {
+    LazyColumn {
+      stickyHeader {
+        Row(
+          modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xff000000))
+            .padding(10.dp)
+        ) {
+          Text(
+            "Messages",
+            color = Color.White,
+            style = MaterialTheme.typography.titleLarge
+          )
+        }
+      }
+      items(messages) { message ->
+        MessageRow(message)
+      }
+    }
+
+    Row(modifier = Modifier
+      .align(Alignment.BottomCenter)
+      .background(Color(0xfff8f8d0))
+      .padding(10.dp)
+    ) {
+      Text("Sticky Footer")
     }
   }
 }
 
 
 @Composable
-fun MessageRow(message: Message,
-               modifier: Modifier = Modifier) {
+fun MessageRow(
+  message: Message,
+  modifier: Modifier = Modifier
+) {
   Log.i(TAG, "Composing MessageRow ${message.index}")
-  Row(modifier = modifier.fillMaxWidth() ) {
+  Row(modifier = modifier.fillMaxWidth()) {
     Text(
       message.text,
       style = MaterialTheme.typography.titleLarge
@@ -54,7 +86,7 @@ fun MessageRow(message: Message,
 fun MessageListPreview() {
   DemoWeek4Theme {
     Surface {
-       MessageList(sampleMessages())
+      MessageList(sampleMessages())
     }
   }
 }
---
+++ DemoWeek42/app/src/main/java/dev/csse/kubiak/demoweek4/ui/ToDoScreen.kt
@@ -0,0 +1,139 @@
+package dev.csse.kubiak.demoweek4.ui
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.SwipeToDismissBoxState
+import androidx.compose.material3.Text
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.csse.kubiak.demoweek4.Task
+import dev.csse.kubiak.demoweek4.ui.theme.DemoWeek4Theme
+
+@Composable
+fun ToDoScreen(
+   modifier: Modifier = Modifier
+) {
+  TaskList( modifier = modifier.fillMaxSize() )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun TaskList(
+   modifier: Modifier = Modifier,
+   todoViewModel: ToDoViewModel = viewModel()
+) {
+   LazyColumn( modifier = modifier ) {
+      stickyHeader {
+         AddTaskInput { s -> todoViewModel.addTask(s) }
+      }
+      items(
+         items = todoViewModel.taskList
+      ) { task ->
+        TaskCard(
+          task = task,
+          toggleCompleted = { t ->
+             todoViewModel.toggleTaskCompleted(t) }
+        )
+      }
+   }
+}
+
+@Composable
+fun AddTaskInput(onEnterTask: (String) -> Unit) {
+   val keyboardController = LocalSoftwareKeyboardController.current
+   var taskBody by remember { mutableStateOf("") }
+
+   OutlinedTextField(
+      modifier = Modifier
+         .fillMaxWidth()
+         .background(Color.White)
+         .padding(6.dp),
+      value = taskBody,
+      onValueChange = { taskBody = it },
+      label = { Text("Enter task") },
+      keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+      keyboardActions = KeyboardActions(
+         onDone = {
+            onEnterTask(taskBody)
+            taskBody = ""
+            keyboardController?.hide()
+         }
+      )
+   )
+}
+
+@Composable
+fun TaskCard(
+   task: Task,
+   toggleCompleted: (Task) -> Unit,
+   modifier: Modifier = Modifier
+) {
+   Card(
+      modifier = modifier
+         .padding(8.dp)
+         .fillMaxWidth(),
+      colors = CardDefaults.cardColors(
+         containerColor = MaterialTheme.colorScheme.surfaceVariant
+      )
+   ) {
+      Row(modifier = modifier.fillMaxWidth(),
+         verticalAlignment = Alignment.CenterVertically,
+         horizontalArrangement = Arrangement.spacedBy(12.dp)
+      ) {
+         Checkbox(
+            checked = task.completed,
+            onCheckedChange = {
+               toggleCompleted(task)
+            }
+         )
+         Text(
+            text = task.body,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier.padding(12.dp),
+            color = if (task.completed) Color.Gray else Color.Black
+         )
+      }
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoScreenPreview() {
+   val todoViewModel = viewModel<ToDoViewModel>()
+   todoViewModel.createTestTasks(5)
+
+   DemoWeek4Theme(dynamicColor = false ) {
+      ToDoScreen()
+   }
+}
