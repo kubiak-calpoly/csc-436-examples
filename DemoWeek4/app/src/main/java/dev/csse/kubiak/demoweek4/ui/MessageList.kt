3e78f3cb1dc14436c6a26be6f29651bd45e7f5cc
Ken Kubiak <kubiak@calpoly.edu>
2025-01-27 11:44:55 -0800

    first demo - non-lazy messagelist

--- DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
+++ DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
@@ -1,33 +1,22 @@
 package dev.csse.kubiak.demoweek4.ui
 
 import android.R
+import android.nfc.Tag
+import android.telephony.TelephonyCallback
 import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemGesturesPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.csse.kubiak.demoweek4.ui.theme.DemoWeek4Theme
-import java.nio.file.WatchEvent
 
 data class Message(
   var text: String = "",
@@ -36,65 +25,41 @@ data class Message(
 
 val TAG = "MessageList"
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MessageList(messages: List<Message>) {
-  Box(modifier = Modifier.fillMaxSize() ) {
-    LazyColumn(
-      modifier = Modifier.fillMaxWidth()
-        .background(Color(0xFF424832))
-    ) {
-      stickyHeader {
-        Row(
-          modifier = Modifier.fillMaxWidth()
-            .border(1.dp, Color(0xFF000000))
-            .background(Color(0xFFF8F8D0))
-            .padding(10.dp)
-        ) {
-
-          Text(
-            "Messages",
-            style = MaterialTheme.typography.titleLarge
-          )
-        }
-
-      }
-      items(messages) { message ->
-        MessageRow(message)
-      }
-    }
-    Row(modifier = Modifier
-      .align(Alignment.BottomCenter )
-      .border(1.dp, Color(0xFF000000))
-      .background(Color(0xFFF8F8D0))
-      .padding(10.dp)) {
-      Text("Sticky Footer")
+  Column {
+    messages.forEach { message ->
+      MessageRow(message)
     }
   }
 }
 
 
 @Composable
-fun MessageRow(message: Message) {
+fun MessageRow(message: Message,
+               modifier: Modifier = Modifier) {
   Log.i(TAG, "Composing MessageRow ${message.index}")
-  Text(message.text,
-    style = MaterialTheme.typography.titleLarge,
-    modifier = Modifier.fillMaxWidth()
-      .padding(5.dp)
-      .background(Color.White)
-      .padding(5.dp))
+  Row(modifier = modifier.fillMaxWidth() ) {
+    Text(
+      message.text,
+      style = MaterialTheme.typography.titleLarge
+    )
+  }
 }
 
 @Preview
 @Composable
 fun MessageListPreview() {
-  val messages = Array<Message>(200) {
-      n -> Message("This is message $n", n)
-  }
-
   DemoWeek4Theme {
     Surface {
-      MessageList(listOf(*messages))
+       MessageList(sampleMessages())
     }
   }
+}
+
+fun sampleMessages(): List<Message> {
+  val messages = Array<Message>(200) { n ->
+    Message("This is message $n", n)
+  }
+  return listOf(*messages)
 }
