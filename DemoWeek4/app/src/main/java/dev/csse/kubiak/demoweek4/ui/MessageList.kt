ff06162978f374cdb21f2afeb9661a737ec83501
Ken Kubiak <kubiak@calpoly.edu>
2025-01-27 11:51:48 -0800

    lazy message list

--- DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
+++ DemoWeek4/app/src/main/java/dev/csse/kubiak/demoweek4/ui/MessageList.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -27,8 +29,8 @@ val TAG = "MessageList"
 
 @Composable
 fun MessageList(messages: List<Message>) {
-  Column {
-    messages.forEach { message ->
+  LazyColumn {
+    items(messages) { message ->
       MessageRow(message)
     }
   }
