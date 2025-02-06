f3a10c8ea742bec174633fee60a394928efadd52
Ken Kubiak <kubiak@calpoly.edu>
2025-02-06 12:25:23 -0800

    Implement ACTION_VIEW intent with website link and geo link

--- DemoWeek5/app/src/main/java/dev/csse/kubiak/demoweek5/Pie.kt
+++ DemoWeek5/app/src/main/java/dev/csse/kubiak/demoweek5/Pie.kt
@@ -5,5 +5,8 @@ data class Pie(
   val name: String = "Mystery Pie",
   val resourceId: Int = R.drawable.apple_pie,
   val crust: String = "shortcrust",
-  val filling: String = "mystery"
+  val filling: String = "mystery",
+  val website: String = "https://en.wikipedia.org/wiki/Apple_pie",
+  val pieShop: String = "Bramble Pie Company",
+  val location: String = "geo:35.489596,-120.668665?q=Bramble%20Pie%20Company"
 )
--- DemoWeek5/app/src/main/java/dev/csse/kubiak/demoweek5/ui/PieDetailScreen.kt
+++ DemoWeek5/app/src/main/java/dev/csse/kubiak/demoweek5/ui/PieDetailScreen.kt
@@ -1,21 +1,32 @@
 package dev.csse.kubiak.demoweek5.ui
 
+import android.R
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -27,6 +38,8 @@ fun PieDetailScreen(
   pie: Pie,
   modifier: Modifier = Modifier
 ) {
+  val context = LocalContext.current
+
   Column(
     modifier = modifier.fillMaxWidth(),
     horizontalAlignment = Alignment.CenterHorizontally,
@@ -40,17 +53,62 @@ fun PieDetailScreen(
       painter = painterResource(pie.resourceId),
       contentDescription = pie.name
     )
-    Card(modifier = Modifier
-      .weight(1f)
-      .padding(20.dp)) {
+    Card(
+      modifier = Modifier
+        .weight(1f)
+        .padding(20.dp)
+    ) {
       Column(modifier = Modifier.padding(8.dp)) {
         Row {
-          Text("Filling", modifier = Modifier.width(60.dp))
-          Text(pie.filling, modifier = Modifier.weight(1f))
+          Text("Filling", modifier = Modifier.width(80.dp))
+          Text(
+            pie.filling, modifier = Modifier.weight(1f), textAlign = TextAlign.End
+          )
         }
         Row {
-          Text("Crust", modifier = Modifier.width(60.dp))
-          Text(pie.crust, modifier = Modifier.weight(1f))
+          Text("Crust", modifier = Modifier.width(80.dp))
+          Text(
+            pie.crust, modifier = Modifier.weight(1f),
+            textAlign = TextAlign.End
+          )
+        }
+        Row {
+          Text("Website", modifier = Modifier.width(80.dp))
+          Box(
+            modifier = Modifier
+              .weight(1f)
+              .clickable(enabled = true,
+                onClick = {
+                  val url = Uri.parse(pie.website)
+                  val intent = Intent(Intent.ACTION_VIEW, url)
+                  context.startActivity(intent)
+                })
+              .background(color = Color(0x40ffffff))
+          ) {
+            Text(pie.name,
+              textAlign = TextAlign.End,
+              modifier = Modifier.align(Alignment.TopEnd)
+            )
+          }
+        }
+        Row {
+          Text("Get π now", modifier = Modifier.width(80.dp))
+          Box(
+            modifier = Modifier
+              .weight(1f)
+              .clickable(enabled = true,
+                onClick = {
+                  val url = Uri.parse(pie.location)
+                  val intent = Intent(Intent.ACTION_VIEW, url)
+                  context.startActivity(intent)
+                })
+              .background(color = Color(0x40ffffff))
+          ) {
+            Text(pie.pieShop,
+              textAlign = TextAlign.End,
+              modifier = Modifier.align(Alignment.TopEnd)
+            )
+          }
         }
       }
     }
