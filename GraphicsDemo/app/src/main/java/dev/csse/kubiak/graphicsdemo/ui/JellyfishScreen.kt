f543eb4edf1022ede270ce986ed091ece748daec
Ken Kubiak <kubiak@calpoly.edu>
2025-02-27 09:19:38 -0800

    make eyes blink on tapping face

--- GraphicsDemo/app/src/main/java/dev/csse/kubiak/graphicsdemo/ui/JellyfishScreen.kt
+++ GraphicsDemo/app/src/main/java/dev/csse/kubiak/graphicsdemo/ui/JellyfishScreen.kt
@@ -1,6 +1,8 @@
 package dev.csse.kubiak.graphicsdemo.ui
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
@@ -8,9 +10,12 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.center
@@ -22,16 +27,20 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.Group
 import androidx.compose.ui.graphics.vector.Path
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.csse.kubiak.graphicsdemo.ui.theme.GraphicsDemoTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun JellyfishScreen(
   model: JellyfishViewModel = viewModel()
 ) {
+
 
   val vectorPainter = rememberVectorPainter(
     defaultWidth = 530.46f.dp,
@@ -43,73 +52,118 @@ fun JellyfishScreen(
     val duration = 3000
     val transition = rememberInfiniteTransition()
     val translationY by transition.animateFloat(
-      initialValue = 0f,
-      targetValue = -30f,
-      animationSpec = infiniteRepeatable(
-        tween(duration, easing = EaseInOut),
-        repeatMode = RepeatMode.Reverse
+      initialValue = 0f, targetValue = -30f, animationSpec = infiniteRepeatable(
+        tween(duration, easing = EaseInOut), repeatMode = RepeatMode.Reverse
       )
     )
 
     Group(
-      name = "jellyfish",
-      translationY = translationY
+      name = "jellyfish", translationY = translationY
     ) {
       Group("tentacles") {
         val alphas = arrayOf(.49f, .66f, .44f, .6f)
         model.tentacles.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
+          Path(pathData = path,
             fill = SolidColor(Color.White),
-            fillAlpha = alphas.getOrElse(i) { 1f }
-          )
+            fillAlpha = alphas.getOrElse(i) { 1f })
         }
       }
       Group(name = "body") {
         val alphas = arrayOf(1f, .5f)
         model.body.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
+          Path(pathData = path,
             fill = SolidColor(Color.White),
-            fillAlpha = alphas.getOrElse(i) { 1f }
-          )
+            fillAlpha = alphas.getOrElse(i) { 1f })
         }
       }
       Group(name = "freckles") {
         // freckle paths
         model.freckles.forEachIndexed { i, path ->
           Path(
-            pathData = path,
-            fill = SolidColor(
+            pathData = path, fill = SolidColor(
               Color(0xfff0dfe2)
-            ),
-            fillAlpha = 1f
-          )
-        }
-
-      }
-      Group(name = "face") {
-        // face paths
-        val colors = arrayOf(Color(0xFFb4bebf), Color(0xFFb4bebf), Color(0xFFd3d3d3))
-        model.face.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
-            fill = SolidColor(colors.getOrElse(i) { Color.Black }),
-            fillAlpha = 1f
+            ), fillAlpha = 1f
           )
         }
       }
     }
+
     Group(name = "bubbles") {
       // bubbles around the jellyfish
       val alphas = arrayOf(.67f, .79f, .89f, .77f, .77f)
       model.bubbles.forEachIndexed { i, path ->
-        Path(
-          pathData = path,
+        Path(pathData = path,
           fill = SolidColor(Color.White),
-          fillAlpha = alphas.getOrElse(i) { .49f }
+          fillAlpha = alphas.getOrElse(i) { .49f })
+      }
+    }
+  }
+
+  val blinkAlphaAnimation = remember {
+    Animatable(1f)
+  }
+  val blinkScaleAnimation = remember {
+    Animatable(1f)
+  }
+
+  suspend fun instantBlinkAnimation() {
+    val tweenSpec = tween<Float>(150, easing = LinearEasing)
+    coroutineScope {
+      launch {
+        blinkAlphaAnimation.animateTo(0f, animationSpec = tweenSpec)
+        blinkAlphaAnimation.animateTo(1f, animationSpec = tweenSpec)
+      }
+      launch {
+        blinkScaleAnimation.animateTo(0.3f, animationSpec = tweenSpec)
+        blinkScaleAnimation.animateTo(1f, animationSpec = tweenSpec)
+      }
+    }
+  }
+
+  val vectorPainterFace = rememberVectorPainter(
+    defaultWidth = 530.46f.dp,
+    defaultHeight = 563.1f.dp,
+    viewportWidth = 530.46f,
+    viewportHeight = 563.1f,
+    autoMirror = true,
+  ) { _, _ ->
+    val duration = 3000
+    val transition = rememberInfiniteTransition()
+    val translationY by transition.animateFloat(
+      initialValue = 0f, targetValue = -30f, animationSpec = infiniteRepeatable(
+        tween(duration, easing = EaseInOut), repeatMode = RepeatMode.Reverse
+      )
+    )
+    Group(name = "face",
+      translationY = translationY) {
+      // face paths
+      Group(
+        name = "eye-left",
+        scaleY = blinkScaleAnimation.value,
+        pivotY = 233f // vertical center of eye path
+      ) {
+        Path(
+          pathData = model.face[0],
+          fill = SolidColor(Color(0xFFb4bebf)),
+          fillAlpha = blinkAlphaAnimation.value,
         )
       }
+      Group(
+        name = "eye-right",
+        scaleY = blinkScaleAnimation.value,
+        pivotY = 233f // vertical center of eye path
+      ) {
+        Path(
+          pathData = model.face[1],
+          fill = SolidColor(Color(0xFFb4bebf)),
+          fillAlpha = blinkAlphaAnimation.value,
+        )
+      }
+      Path(
+        pathData = model.face[2],
+        fill = SolidColor(Color(0xFFd3d3d3)),
+        fillAlpha = 1f,
+      )
     }
   }
 
@@ -120,6 +174,19 @@ fun JellyfishScreen(
       .fillMaxSize()
       .background(largeRadialGradient)
   )
+
+  val coroutineScope = rememberCoroutineScope()
+  Image(vectorPainterFace,
+    contentDescription = "",
+    modifier = Modifier
+      .fillMaxSize()
+      .pointerInput(Unit) {
+        detectTapGestures {
+          coroutineScope.launch {
+            instantBlinkAnimation()
+          }
+        }
+      })
 }
 
 @Preview
