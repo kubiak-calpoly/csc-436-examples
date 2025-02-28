9649f6398f15fafcfee1031c79591effb119d652
Ken Kubiak <kubiak@calpoly.edu>
2025-02-28 10:41:32 -0800

    clean start for jellyfish demo

--- GraphicsDemo/app/src/main/java/dev/csse/kubiak/graphicsdemo/ui/JellyfishScreen.kt
+++ GraphicsDemo/app/src/main/java/dev/csse/kubiak/graphicsdemo/ui/JellyfishScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.Group
 import androidx.compose.ui.graphics.vector.Path
+import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,9 +23,7 @@ import dev.csse.kubiak.graphicsdemo.ui.theme.GraphicsDemoTheme
 
 
 @Composable
-fun JellyfishScreen(
-  model: JellyfishViewModel = viewModel()
-) {
+fun JellyfishScreen() {
 
   val vectorPainter = rememberVectorPainter(
     defaultWidth = 530.46f.dp,
@@ -33,65 +32,7 @@ fun JellyfishScreen(
     viewportHeight = 563.1f,
     autoMirror = true,
   ) { _, _ ->
-    Group(
-      name = "jellyfish"
-    ) {
-      Group("tentacles") {
-        val alphas = arrayOf(.49f, .66f, .44f, .6f)
-        model.tentacles.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
-            fill = SolidColor(Color.White),
-            fillAlpha = alphas.getOrElse(i) { 1f }
-          )
-        }
-      }
-      Group(name = "body") {
-        val alphas = arrayOf(1f, .5f)
-        model.body.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
-            fill = SolidColor(Color.White),
-            fillAlpha = alphas.getOrElse(i) { 1f }
-          )
-        }
-      }
-      Group(name = "freckles") {
-        // freckle paths
-        model.freckles.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
-            fill = SolidColor(
-              Color(0xfff0dfe2)
-            ),
-            fillAlpha = 1f
-          )
-        }
 
-      }
-      Group(name = "face") {
-        // face paths
-        val colors = arrayOf(Color(0xFFb4bebf), Color(0xFFb4bebf), Color(0xFFd3d3d3))
-        model.face.forEachIndexed { i, path ->
-          Path(
-            pathData = path,
-            fill = SolidColor(colors.getOrElse(i) { Color.Black }),
-            fillAlpha = 1f
-          )
-        }
-      }
-    }
-    Group(name = "bubbles") {
-      // bubbles around the jellyfish
-      val alphas = arrayOf(.67f, .79f, .89f, .77f, .77f)
-      model.bubbles.forEachIndexed { i, path ->
-        Path(
-          pathData = path,
-          fill = SolidColor(Color.White),
-          fillAlpha = alphas.getOrElse(i) { .49f }
-        )
-      }
-    }
   }
 
   Image(
@@ -99,7 +40,7 @@ fun JellyfishScreen(
     contentDescription = "Jellyfish",
     modifier = Modifier
       .fillMaxSize()
-      .background(largeRadialGradient)
+      .background(Color.Blue)
   )
 }
 
