8c67eae5b918e1f97cbecc8922efeb6a420d40fa
Ken Kubiak <kubiak@calpoly.edu>
2025-03-03 11:28:03 -0800

    add servuce

--- DemoWeek9/app/src/main/java/dev/csse/kubiak/demoweek9/data/WeatherService.kt
+++ DemoWeek9/app/src/main/java/dev/csse/kubiak/demoweek9/data/WeatherService.kt
@@ -5,10 +5,11 @@ import retrofit2.http.Query
 
 interface WeatherService {
 
+  @GET("weather?units=imperial")
   suspend fun getWeather(
-    lat: Float,
-    lon: Float,
-    apiKey: String
+    @Query("lat") lat: Float,
+    @Query("lon") lon: Float,
+    @Query("appid") apiKey: String
   ): WeatherReport
 
 }
