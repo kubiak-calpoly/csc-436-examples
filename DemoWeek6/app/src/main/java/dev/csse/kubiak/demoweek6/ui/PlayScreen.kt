ed65ac6265bddc79178ccfbfa57376e84e55ab54
Ken Kubiak <kubiak@calpoly.edu>
2025-10-26 16:10:04 -0700

    update display as position changes

--- DemoWeek6/app/src/main/java/dev/csse/kubiak/demoweek6/ui/PlayScreen.kt
+++ DemoWeek6/app/src/main/java/dev/csse/kubiak/demoweek6/ui/PlayScreen.kt
@@ -1,5 +1,7 @@
 package dev.csse.kubiak.demoweek6.ui
 
+import android.util.Log
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -29,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -47,6 +50,8 @@ fun PlayScreen(
   playerViewModel: PlayerViewModel = viewModel()
 ) {
   val position = playerViewModel.getPosition(loop)
+
+  Log.d("PlayScreen", "Position: $position")
 
   Column(modifier = modifier.fillMaxWidth()) {
     Row(modifier = Modifier.fillMaxWidth()) {
@@ -76,11 +81,15 @@ fun PlayScreen(
       verticalAlignment = Alignment.CenterVertically
     ) {
       loop.beats.forEach {
-        Beat(it)
         if (it.bar > 1 && it.beat == 1) {
           Box(modifier = Modifier.width(1.dp)
             .fillMaxHeight().background(Color(0xff000000)))
         }
+        Beat(it,
+          hasBeenPlayed = it.bar < position.bar ||
+                  it.bar == position.bar && it.beat <= position.beat,
+          isPlaying = it.bar == position.bar && it.beat == position.beat
+        )
       }
     }
     Row(
