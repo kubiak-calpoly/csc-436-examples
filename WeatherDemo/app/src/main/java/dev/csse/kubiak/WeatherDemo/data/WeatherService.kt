01c9e18da4d3525d4dfc95a8927b5013700c520c
Ken Kubiak <kubiak@calpoly.edu>
2025-11-06 14:12:43 -0800

    remove Retrofit annotations on WeatherService

--- WeatherDemo/app/src/main/java/dev/csse/kubiak/WeatherDemo/data/WeatherService.kt
+++ WeatherDemo/app/src/main/java/dev/csse/kubiak/WeatherDemo/data/WeatherService.kt
@@ -7,11 +7,12 @@ import retrofit2.http.Query
 
 interface WeatherService {
 
-  @GET("weather?units=imperial")
+  // TODO: @GET
   suspend fun getWeather(
-    @Query("lat") lat: Float,
-    @Query("lon") lon: Float,
-    @Query("appid") apiKey: String
+    // TODO: @Query for each query param
+    lat: Float,
+    lon: Float,
+    apiKey: String
   ): WeatherReport
 }
 
